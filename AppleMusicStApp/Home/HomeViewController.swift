8063ed7ceb970dd8c7cb3a2740257ffbcbe0257b
김경도 <49026505+rudeh567@users.noreply.github.com>
2021-09-30 14:41:15 +0900

    TrackManger 구현

---
+++ AppleMusicStApp/Home/HomeViewController.swift
@@ -0,0 +1,64 @@
+//
+//  HomeViewController.swift
+//  AppleMusicStApp
+//
+//  Created by yeoboya on 2021/09/30.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    // TODO: 트랙관리 객체 추가
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
+extension HomeViewController: UICollectionViewDataSource {
+    // 몇개 표시 할까?
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // TODO: 트랙매니저에서 트랙갯수 가져오기
+        return 10
+    }
+    
+    // 셀 어떻게 표시 할까?
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // 셀 구성하기
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TrackCollectionViewCell", for: indexPath) as? TrackCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        return UICollectionViewCell()
+    }
+    
+    // 헤더뷰 어떻게 표시할까?
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            // TODO: 헤더 구성하기
+            return UICollectionReusableView()
+        default:
+            return UICollectionReusableView()
+        }
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegate {
+    // 클릭했을때 어떻게 할까?
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // TODO: 곡 클릭시 플레이어뷰 띄우기
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    // 셀 사이즈 어떻게 할까?
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // 20 - card(width) - 20 - card(width) - 20
+        // TODO: 셀 사이즈 구하기
+        return CGSize.zero
+    }
+}
---
+++ AppleMusicStApp/Home/TrackCollectionViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TrackCollectionViewCell.swift
+//  AppleMusicStApp
+//
+//  Created by yeoboya on 2021/09/30.
+//
+
+import UIKit
+
+class TrackCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var trackThumbnail: UIImageView!
+    @IBOutlet weak var trackTitle: UILabel!
+    @IBOutlet weak var trackArtist: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        trackThumbnail.layer.cornerRadius = 4
+        trackArtist.textColor = UIColor.systemGray2
+    }
+    
+    func updateUI(item: Track?) {
+        // TODO: 곡정보 표시하기
+        
+    }
+    
+}
